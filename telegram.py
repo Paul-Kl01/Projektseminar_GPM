ebc54d72694c42b8ebf109cd9f10dccac83a3fb4
Paul-Kl01 <kluge.paul@gmx.de>
2023-11-28 15:24:13 +0100

    Telegram Api connection

---
+++ telegram.py
@@ -0,0 +1,16 @@
+import telebot
+
+with open("token.txt") as file:
+    token = file.read()
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
