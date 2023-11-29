c7e704d19081eeb17c7cbef1bf36e41d623aeb15
Paul-Kl01 <kluge.paul@gmx.de>
2023-11-29 14:50:53 +0100

    Klasse erstellt, muss weiter unterteilt werden

--- api_call_nina.py
+++ api_call_nina.py
@@ -3,73 +3,79 @@ import requests
 import pandas as pd
 from datetime import datetime
 from datetime import timezone
-## Imports ## 
 
-## Api Variablen für NINA
-api_variablen = {
-    "hochwasser": "/lhp/mapData",
-    "polizei": "/police/mapData",
-    "wetter": "/dwd/mapData",
-    "katwarn": "/katwarn/mapData",
-    "mowas": "/mowas/mapData"
-}
+class ApiCall: 
+    def getData(self):
+        ## Api Variablen für NINA
+        api_variablen = {
+            "hochwasser": "/lhp/mapData",
+            "polizei": "/police/mapData",
+            "wetter": "/dwd/mapData",
+            "katwarn": "/katwarn/mapData",
+            "mowas": "/mowas/mapData"
+        }
 
-# Dataframe Collumns definieren  
-columns = ['ID', 'Urgency','Area', 'Titel', 'Event', 'Datum']
+        # Dataframe Collumns definieren  
+        columns = ['ID', 'Urgency','Area', 'Titel', 'Event', 'Datum']
 
-# NINA Api URLs 
-ninaBaseUrl = "https://warnung.bund.de/api31"
-ninaWarningsUrl = "https://nina.api.proxy.bund.dev/api31/warnings/"
+        # NINA Api URLs 
+        ninaBaseUrl = "https://warnung.bund.de/api31"
+        ninaWarningsUrl = "https://nina.api.proxy.bund.dev/api31/warnings/"
 
-# Einzelne NINA Warnings abfragen 
-def get_api_warning(meldung):
-  response = requests.get(ninaBaseUrl+meldung+".json")
-  return response.json()
+        # Einzelne NINA Warnings abfragen 
+        def get_api_warning(meldung):
+            response = requests.get(ninaBaseUrl+meldung+".json")
+            return response.json()
 
-# Details zu NINA Warning abfragen: Return DataFrame mit allen Ergebnissen 
-def get_api_details(warning):
-    n = 0
-    response = get_api_warning(warning)
-    df = pd.DataFrame(columns=columns)
-    # GetDetails for warning 
-    for responses in response:
-        id = responses["id"]
-        warningDetails = requests.get(ninaWarningsUrl+id+".json").json() 
-        meldungsText = warningDetails["info"][0]["headline"]+ ": "+warningDetails["info"][0]["description"]
-        warnung = warningDetails["info"][0]["event"]
-        start = warningDetails["sent"]
-        urgency = warningDetails["info"][0]["urgency"]
-        area = warningDetails["info"][0]["area"][0]["areaDesc"]
-        d = datetime.fromisoformat(start).astimezone(timezone.utc)
-        d.strftime('%Y-%m-%d %H:%M:')
-        time = d.replace(tzinfo=None)
-        df.loc[n] = [id, urgency, area, meldungsText, warnung, time]
-        n = n+1
-    return df
+        # Details zu NINA Warning abfragen: Return DataFrame mit allen Ergebnissen 
+        def get_api_details(warning):
+            n = 0
+            response = get_api_warning(warning)
+            df = pd.DataFrame(columns=columns)
+            # GetDetails for warning 
+            for responses in response:
+                id = responses["id"]
+                warningDetails = requests.get(ninaWarningsUrl+id+".json").json() 
+                meldungsText = warningDetails["info"][0]["headline"]+ ": "+warningDetails["info"][0]["description"]
+                warnung = warningDetails["info"][0]["event"]
+                start = warningDetails["sent"]
+                urgency = warningDetails["info"][0]["urgency"]
+                area = warningDetails["info"][0]["area"][0]["areaDesc"]
+                d = datetime.fromisoformat(start).astimezone(timezone.utc)
+                d.strftime('%Y-%m-%d %H:%M:')
+                time = d.replace(tzinfo=None)
+                df.loc[n] = [id, urgency, area, meldungsText, warnung, time]
+                n = n+1
+            return df
 
-## Dataframe to CSV 
-def df_to_csv(data, filename): 
-    data.to_csv(filename+'.csv') 
- 
-# Dataframe definieren    
-data = pd.DataFrame(columns=columns) 
+        ## Dataframe to CSV 
+        def df_to_csv(data, filename): 
+            data.to_csv(filename+'.csv') 
+        
+        # Dataframe definieren    
+        data = pd.DataFrame(columns=columns) 
 
-## API abfragen für alle NINA Variablen/Warnings ##
-for x in api_variablen:
-    data_api = get_api_details(api_variablen[x])
-    data = pd.concat([data, data_api])
-    print(data)
+        ## API abfragen für alle NINA Variablen/Warnings ##
+        for x in api_variablen:
+            data_api = get_api_details(api_variablen[x])
+            data = pd.concat([data, data_api])
 
-# DataFrame df2 erstellen
-data['Area'] = data['Area'].str.split(', ')
-df2 = data.explode('Area')
-print(df2)
+        # DataFrame df2 erstellen
+        data['Area'] = data['Area'].str.split(', ')
+        df2 = data.explode('Area')
 
-# API Daten in CSV speichern 
-df_to_csv(df2, "Data")
+        # API Daten in CSV speichern 
+        df_to_csv(df2, "Data")
 
-# DataFrame ausgeben
-print(df2)
+        # DataFrame ausgeben
+        print(df2) 
+        
+        # Return 
+        return df2
+
+# Testing    
+l1 = ApiCall()
+l1.getData()
 
 
 
