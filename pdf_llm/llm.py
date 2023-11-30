9f198b782c839db397e57aa8f5b99728e49c8a24
MaxGit32 <146948112+MaxGit32@users.noreply.github.com>
2023-11-30 12:08:36 +0100

    Update llm.py

--- pdf_llm/llm.py
+++ pdf_llm/llm.py
@@ -5,25 +5,14 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import DirectoryLoader, PyPDFLoader
 import os
 from PyPDF2 import PdfReader
-<<<<<<< HEAD
 from langchain.chains import RetrievalQAWithSourcesChain
-##################### Max ##########################
+###########
 #pip install faiss-cpu
 #pip install langchain
 #pip install pypdf
 #pip tiktoken
 #pip install InstructorEmbedding
-
-#loader = DirectoryLoader("/home/max/Dokumente/5.\ Semester/Projektseminar/Projektseminar_GPM/pdf_llm/pdf", loader_cls=PyPDFLoader)
-#pages = loader.load_and_split()
-#text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-#docs = text_splitter.split_documents(pages)
-=======
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
->>>>>>> ed9489fc1b549be95c32030888413ec51818f39a
-
+###############
 
 # PDF in String umwandeln
 def get_pdf_text(folder_path):
@@ -53,7 +42,7 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-<<<<<<< HEAD
+
 # Aufruf
 folder_path = './PDFs'
 #text_content = get_pdf_text(folder_path)
@@ -66,30 +55,13 @@ embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
 vectorstoreDB = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
 
 ### Similarity-Check mit LLM
-query = "ell"
-docs = vectorstoreDB.similarity_search(query)
-print(docs)
+#query = "ell"
+#docs = vectorstoreDB.similarity_search(query)
+#print(docs)
+
 
 #### Frage und Antwort mit LLM
 #retriever = vectorstoreDB.as_retriever()
 #model = RetrievalQAWithSourcesChain.from_chain_type(llm=)
-##################### Max #########################
-=======
-#Vektorstore erstellen
-def get_vectorstore(text_chunks):
-    #embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
 
-# Aufruf
-folder_path = './PDFs'
-text_content = get_pdf_text(folder_path)
-#print(text_content.replace('\n', ' '))
 
-chunks = get_text_chunks(text_content)
-#print(chunks)
-
-#vekt = get_vectorstore(chunks)
-#print(vekt)
->>>>>>> ed9489fc1b549be95c32030888413ec51818f39a
