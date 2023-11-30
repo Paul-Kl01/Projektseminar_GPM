46c1c036d87e2fa9a7011cd16cdccdf4ce716143
Paul-Kl01 <kluge.paul@gmx.de>
2023-11-30 15:48:17 +0100

    Telebot async eingefügt (Julian)

--- telegram.py
+++ telegram.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-
+### Paul alt ####
 with open("token.txt") as file:
     token = file.read()
 
@@ -45,3 +45,33 @@ def get_Message(message):
 
 # Bot starten 
 bot.infinity_polling()
+
+
+
+#### Julian neu #####
+from telebot.async_telebot import AsyncTeleBot
+bot = AsyncTeleBot('TOKEN')
+token = ""
+with open("token.txt") as file:
+    token = file.read()
+
+bot =  AsyncTeleBot("TOKEN Einfügen")
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, """\
+            Hi there, I am EchoBot.
+            I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+            """)
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    
+    await bot.reply_to(message, message.text)
+
+
+import asyncio
+asyncio.run(bot.polling())
