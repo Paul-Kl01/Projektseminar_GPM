fe50d7539ef58173df7fe28590bdb4528a581939
name <me@example.com>
2023-12-01 12:31:47 +0100

    updates

--- pdf_llm/llm-version2.py
+++ pdf_llm/llm-version2.py
@@ -1,12 +1,6 @@
 
 from langchain.embeddings import HuggingFaceInstructEmbeddings 
 from langchain.vectorstores import FAISS
-
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
-from langchain.chat_models import ChatAnthropic
-from transformers import pipeline
 ###########
 #pip install faiss-cpu
 #pip install langchain
@@ -15,7 +9,6 @@ from transformers import pipeline
 #pip install InstructorEmbedding
 ###############
 
-
 def get_vectorstore():
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
     #Abruf lokaler Vektordatenbank
@@ -23,42 +16,15 @@ def get_vectorstore():
     vectorstoreDB = FAISS.load_local(save_directory, embeddings)
     return vectorstoreDB
 
-
 def main():
-    #user_question = "Wie lautet deine Frage?"
+    user_question = ################
 
     retriever=get_vectorstore().as_retriever()
     retrieved_docs=retriever.invoke(
-    "Wie entsteht Hochwasser?"
+    user_question
     )
     print(retrieved_docs[0].page_content)
-    #def format_docs(docs):
-    #    return "\n\n".join([d.page_content for d in docs])
-
-    #template = """Answer the question based only on the following context:
-
-    #{context}
-
-    #Question: {question}
-    #"""
-    #prompt = ChatPromptTemplate.from_template(template)
-    #model = ChatAnthropic()
-    #chain = (
-    #{"context": retriever | format_docs, "question": RunnablePassthrough()}
-    #| prompt
-    #| model
-    #| StrOutputParser()
-    #)
-    #print(chain.invoke("Was macht man im Katastrophenfall?"))
-    #create_vectorstore_and_store(text_chunks)      # bei incoming pdf
-
-    #vectorstore_DB=get_vectorstore()        # bei Abfrage durch Chatbot
-    #print(get_vectorstore().similarity_search_with_score("stelle")) # zeigt an ob Vektordatenbank gefüllt ist
     
-    #print(get_conversation_chain(get_vectorstore()))
-
-    #text2text_generator = pipeline("text2text-generation", model="twigs/bart-text2text-simplifier")
-    #print(text2text_generator(retrieved_docs[0].page_content, max_length= 300))
 
 if __name__ == '__main__':
     main()
