7addeda9ebc1174a943bbec28209c9f3e77af91e
name <me@example.com>
2023-12-01 12:05:10 +0100

    updates

--- pdf_llm/llm-version2.py
+++ pdf_llm/llm-version2.py
@@ -6,11 +6,16 @@ from langchain.document_loaders import DirectoryLoader, PyPDFLoader
 import os
 from PyPDF2 import PdfReader
 from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+#from langchain.memory import ConversationBufferMemory
+#from langchain.chains import ConversationalRetrievalChain
 #from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 from dotenv import load_dotenv
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.chat_models import ChatAnthropic
+from transformers import pipeline
 ###########
 #pip install faiss-cpu
 #pip install langchain
@@ -49,23 +54,19 @@ def get_text_chunks(text):
 
 # nur zum Anlegen des lokalen Verzeichnisses "Store" und speichern der Vektor-Datenbank
 def create_vectorstore_and_store(text_chunks):
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
     # Initiate Faiss DB
     vectorstoreDB = FAISS.from_texts(texts=text_chunks,embedding=embeddings)#texts=text_chunks,
-    ###
-    ### --> danach soll das PDF-Verzeichnis gelöscht werden, bzw. Datein verschieben, weil beim nächsten Upload 
-    ###
     # Verzeichnis in dem die VektorDB gespeichert werden soll
     save_directory = "Store"
     #VektorDB lokal speichern
     vectorstoreDB.save_local(save_directory)
     print(vectorstoreDB)
     return None
-    
-########
+
 
 def get_vectorstore():
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
     #Abruf lokaler Vektordatenbank
     save_directory = "Store"
     vectorstoreDB = FAISS.load_local(save_directory, embeddings)
@@ -74,16 +75,36 @@ def get_vectorstore():
 
 def main():
     load_dotenv()
-    user_question = "Wie lautet deine Frage?"
+    #user_question = "Wie lautet deine Frage?"
     folder_path = './PDFs'
     pdf_text = get_pdf_text(folder_path)
     text_chunks = get_text_chunks(pdf_text)
-    #conversation = get_conversation_chain(get_vectorstore())
     retriever=get_vectorstore().as_retriever()
     retrieved_docs=retriever.invoke(
     "Was macht man im Katastrophenfall?"
     )
-    print(retrieved_docs[0].page_content)
+    
+    #def format_docs(docs):
+    #    return "\n\n".join([d.page_content for d in docs])
+
+
+
+
+    #template = """Answer the question based only on the following context:
+
+    #{context}
+
+    #Question: {question}
+    #"""
+    #prompt = ChatPromptTemplate.from_template(template)
+    #model = ChatAnthropic()
+    #chain = (
+    #{"context": retriever | format_docs, "question": RunnablePassthrough()}
+    #| prompt
+    #| model
+    #| StrOutputParser()
+    #)
+    #print(chain.invoke("Was macht man im Katastrophenfall?"))
     #create_vectorstore_and_store(text_chunks)      # bei incoming pdf
 
     #vectorstore_DB=get_vectorstore()        # bei Abfrage durch Chatbot
@@ -91,8 +112,8 @@ def main():
     
     #print(get_conversation_chain(get_vectorstore()))
 
-
-
+    text2text_generator = pipeline("text2text-generation", model="twigs/bart-text2text-simplifier")
+    print(text2text_generator(retrieved_docs[0].page_content))
 
 if __name__ == '__main__':
     main()
